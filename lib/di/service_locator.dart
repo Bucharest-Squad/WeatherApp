72505a499ea6a90a3f07805bf82e90e56ac79764
eslam-99 <eslam.mohamed.551999@gmail.com>
2025-10-22 16:52:16 +0300

    feature: add di

---
+++ lib/di/service_locator.dart
@@ -0,0 +1,21 @@
+import 'package:get_it/get_it.dart';
+
+final GetIt getIt = GetIt.instance;
+
+void setupServiceLocator() {
+  _setupNetworkClient();
+  _setupRepository();
+  _setupCubit();
+}
+
+void _setupRepository() {
+
+}
+
+void _setupNetworkClient() {
+
+}
+
+void _setupCubit() {
+
+}
