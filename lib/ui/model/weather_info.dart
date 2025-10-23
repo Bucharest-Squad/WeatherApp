e1929870245f4860aa1894ba10a938d7c8d44273
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-10-23 20:19:57 +0100

    feature: add weather details card

---
+++ lib/ui/model/weather_info.dart
@@ -0,0 +1,11 @@
+class WeatherInfo {
+  final String iconPath;
+  final String value;
+  final String label;
+
+  const WeatherInfo({
+    required this.iconPath,
+    required this.value,
+    required this.label,
+  });
+}
