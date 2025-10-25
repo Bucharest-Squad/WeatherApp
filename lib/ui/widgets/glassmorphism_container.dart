280e065df327b5ad81b17610a77ca808d63deb32
AhmadKharfan <plamoplamo12@gmail.com>
2025-10-25 20:44:41 +0000

    add more styling options to `GlassmorphismContainer`

--- lib/ui/widgets/glassmorphism_container.dart
+++ lib/ui/widgets/glassmorphism_container.dart
@@ -8,6 +8,9 @@ class GlassmorphismContainer extends StatelessWidget {
   final double borderRadius;
   final double? width;
   final double? height;
+  final Color? backgroundColor;
+  final Color? borderColor;
+  final List<BoxShadow>? shadows;
 
   const GlassmorphismContainer({
     super.key,
@@ -17,26 +20,35 @@ class GlassmorphismContainer extends StatelessWidget {
     this.borderRadius = 30.0,
     this.width,
     this.height,
+    this.backgroundColor,
+    this.borderColor,
+    this.shadows,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(borderRadius),
-      child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: blur, sigmaY: blur),
-        child: Container(
-          width: width,
-          height: height,
-          decoration: BoxDecoration(
-            color: Colors.white.withOpacity(opacity),
-            borderRadius: BorderRadius.circular(borderRadius),
-            border: Border.all(
-              color: Colors.white.withOpacity(0.3),
-              width: 1.5,
+    final effectiveBackgroundColor = backgroundColor ?? Colors.white.withOpacity(opacity);
+    final effectiveBorderColor = borderColor ?? Colors.white.withOpacity(0.3);
+
+    return Container(
+      width: width,
+      height: height,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(borderRadius),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: blur, sigmaY: blur),
+          child: Container(
+            decoration: BoxDecoration(
+              color: effectiveBackgroundColor,
+              borderRadius: BorderRadius.circular(borderRadius),
+              border: Border.all(
+                color: effectiveBorderColor,
+                width: 1.5,
+              ),
+              boxShadow: shadows,
             ),
+            child: child,
           ),
-          child: child,
         ),
       ),
     );
