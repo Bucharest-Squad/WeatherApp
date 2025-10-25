70c7e5f0bd00108ceddc4fdc8005154dd5f63239
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-10-25 14:47:00 +0300

    refactor : solve ui issues

--- lib/ui/components/daily_weather.dart
+++ lib/ui/components/daily_weather.dart
@@ -1,115 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:weather_app/ui/model/daily_weather.dart';
+import 'package:weather_app/ui/theme/font_families.dart';
 import '../theme/app_colors.dart';
 
-class WeaklyWeatherForecastPage extends StatelessWidget {
+class NextDaysWeatherForecastTable extends StatelessWidget {
   final List<DailyWeatherInfo> dailyWeatherList;
-  const WeaklyWeatherForecastPage({super.key, required this.dailyWeatherList});
+
+  const NextDaysWeatherForecastTable ({super.key, required this.dailyWeatherList});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 0,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(24),
-          side: const BorderSide(color: AppColors.dayBorder, width: 1),
-        ),
-         clipBehavior: Clip.antiAlias,
-      child: Container(
+    return Container(
+      decoration: BoxDecoration(
         color: AppColors.dayBackground,
-        child: ListView.separated(
-          padding: EdgeInsets.zero,
-          shrinkWrap: true,
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(24),
+        border: Border.all(color: AppColors.dayBorder, width: 1),
+      ),
+      child: ListView.separated(
+        padding: EdgeInsets.zero,
+        shrinkWrap: true,
 
-          itemCount: dailyWeatherList.length,
+        itemCount: dailyWeatherList.length,
 
-          itemBuilder: (context, index) {
-            return DailyWeatherDetails(dailyWeather: dailyWeatherList[index]);
-          },
+        itemBuilder: (context, index) {
+          return _DailyWeatherDetails(dailyWeather: dailyWeatherList[index]);
+        },
 
-          separatorBuilder: (context, index) {
-            return const Divider(
-              height: 1,
-              thickness: 1,
-              color: AppColors.dayBorder,
-            );
-          },
-        ),
+        separatorBuilder: (context, index) {
+          return const Divider(
+            height: 1,
+            thickness: 1,
+            color: AppColors.dayBorder,
+          );
+        },
       ),
     );
   }
 }
 
-class DailyWeatherDetails extends StatelessWidget {
+class _DailyWeatherDetails extends StatelessWidget {
   final DailyWeatherInfo dailyWeather;
 
-  const DailyWeatherDetails({super.key, required this.dailyWeather});
+  const _DailyWeatherDetails({required this.dailyWeather});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
-    child:  Row(
-      children: <Widget>[
-        Expanded(
-          child: Container(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              dailyWeather.dayOfWeek,
-              textAlign: TextAlign.start,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-                color: AppColors.dayText60,
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                dailyWeather.dayOfWeek,
+                textAlign: TextAlign.start,
+                style: const TextStyle(
+                  fontFamily: urbanist,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  color: AppColors.dayText60,
+                ),
               ),
             ),
           ),
-        ),
 
-        Expanded(
-          child: Center(
-            child: Image.asset(dailyWeather.iconPath, width: 32, height: 32),
+          Expanded(
+            child: Center(
+              child: Image.asset(dailyWeather.iconPath, height: 32),
+            ),
           ),
-        ),
 
-        Expanded(
-          child: Row(
+          Expanded(
+            child: Row(
 
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              _TempDisplay(
-                icon: Icons.arrow_upward,
-                text: '${dailyWeather.maxTemperature}°C',
-              ),
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                _TempDisplay(
+                  icon: 'assets/images/arrow_up.svg',
+                  text: '${dailyWeather.maxTemperature}°C',
+                ),
 
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 4.0),
-                child: Text(
-                  '|',
-                  style: TextStyle(
-                    color: AppColors.daySeparator,
-                    fontWeight: FontWeight.w100,
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 4.0),
+                  child: Text(
+                    '|',
+                    style: TextStyle(
+                      fontFamily: urbanist,
+                      color: AppColors.daySeparator,
+                      fontWeight: FontWeight.w100,
+                    ),
                   ),
                 ),
-              ),
 
-              _TempDisplay(
-                icon: Icons.arrow_downward,
-                text: '${dailyWeather.minTemperature}°C',
-              ),
+                _TempDisplay(
+                  icon: 'assets/images/arrow_up.svg',
+                  text: '${dailyWeather.minTemperature}°C',
+                ),
 
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
-    ),
+        ],
+      ),
     );
   }
 }
 
 class _TempDisplay extends StatelessWidget {
-  final IconData icon;
+  final String icon;
   final String text;
 
   const _TempDisplay({required this.icon, required this.text});
@@ -118,11 +120,16 @@ class _TempDisplay extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Icon(icon, size: 12, color: AppColors.dayText87),
+        SvgPicture.asset(icon, height: 12, colorFilter: ColorFilter.mode(
+          AppColors.dayText87,
+          BlendMode.srcIn,
+        ),
+        ),
         const SizedBox(width: 2.5),
         Text(
           text,
           style: TextStyle(
+            fontFamily: urbanist,
             fontSize: 14,
             color: AppColors.dayText87,
             fontWeight: FontWeight.w500,
